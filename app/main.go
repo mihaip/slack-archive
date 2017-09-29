00ae6953603e50ea6535935975d2fc69cd622a37
Mihai Parparita <mihai@persistent.info>
2017-09-28 21:31:21 -0700

    Add another whitelist entry.

--- app/main.go
+++ app/main.go
@@ -178,7 +178,7 @@ func slackOAuthCallbackHandler(w http.ResponseWriter, r *http.Request) *AppError
 	}
 
 	c := appengine.NewContext(r)
-	if authTest.Team != "Partyslack" && authTest.Team != "Quip" && authTest.Team != "More Partier More Chattier" && authTest.Team != "Partiest Chattiest" && authTest.Team != "DanceDeets" {
+	if authTest.Team != "Partyslack" && authTest.Team != "Quip" && authTest.Team != "More Partier More Chattier" && authTest.Team != "Partiest Chattiest" && authTest.Team != "DanceDeets" && authTest.Team != "Spring '17 Babies" {
 		c.Warningf("Non-whitelisted team %s used", authTest.Team)
 		return templates["team-not-on-whitelist"].Render(w, map[string]interface{}{})
 	}
