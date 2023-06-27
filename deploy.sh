ec73937b0240fa99b5ce5fc299f328c4435b831d
Mihai Parparita <mihai@persistent.info>
2023-06-26 23:06:01 -0700

    Simplify deployment script
    We started to use Go modules (with eddd1fa83366582b081811fe2401372a2420b444)
    and thus don't need to do any hacks before deploying.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-# With the Go 1.11 runtime, if we're not using modules, all source (including
-# the app itself) must live under GOPATH. Copy it there before deploying.
-DEST="$GOPATH/src/slack-archive"
-rm -rf $DEST
-cp -r app $DEST
-cd $DEST
+cd app
 gcloud app deploy --project slack-archive app.yaml queue.yaml
